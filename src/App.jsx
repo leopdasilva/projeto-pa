84f4f0704053edae996d1a399e65ef57a1bf0944
leopdasilva <leonardopereiradasilva774@gmail.com>
2026-06-26 16:37:14 -0300

    Alteração de layout e responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -15,47 +15,65 @@ function App() {
   };
 
   return (
-    <>
-      {/* Header e caixa de Login */}
-      <Header titulo="Lanchonete Xpress" subtitulo="A entrega mais rápida da região"/>
-      <Login login="Login"/>
+    <div className="app-container">
+      {/*Header com imagens para logo e subtitulo*/}
+      <Header/>
 
-      {/* Container com os cards sobre os produtos */}
-      <div className="produtos-container">
-        <h2 className="titulo-cardapio">Cardápio</h2>
-        <div className="produtos-cards-wrapper">
-          <CardProduto nome="X-Burguer" preco="15,90" descricao="Hambúrguer artesanal grelhado, muito queijo prato derretido e maionese especial da casa no pão brioche selado." imagem="https://images.pexels.com/photos/33673825/pexels-photo-33673825.jpeg"/>
-          <CardProduto nome="X-Salada" preco="17,90" descricao="Blend bovino suculento, queijo derretido, alface americana fresca, tomates selecionados e molho artesanal." imagem="https://images.pexels.com/photos/3738730/pexels-photo-3738730.jpeg"/>
-          <CardProduto nome="Refrigerante" preco="6,00" descricao="Lata de 350ml trincando de gelada. Escolha o seu sabor favorito para acompanhar o seu lanche." imagem="https://images.pexels.com/photos/5860659/pexels-photo-5860659.jpeg"/>
-        </div>
-      </div>
-
-      {/* Container com os cards sobre os funcionários */}
-      <div className="funcionarios-container">
-          <h2 className="titulo-funcionarios">Funcionários</h2>
-          <div className="funcionarios-cards-wrapper">
-            <Funcionario nome="Alexandre Gaspari" cargo="Atendente" fotoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPSvxlMQ0yxOc2hXkochHyY51xkZsBxKFDHw&s"/>
-            <Funcionario nome="Cesar Stati" cargo="Gerente" fotoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwU2daGPdMx-ZYsHz8DuHeFBi_cqZtpUGF9Q&s"/>
-            <Funcionario nome="Donathan Goncalves" cargo="Chef de Cozinha" fotoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDxliqPfoR4K-WKT7JoGovyVi794g6FrtKXQ&s"/>
+      {/* Container flex que divide o resto do site em duas partes */}
+      <div className="conteudo-principal">
+        
+        {/* ================= COLUNA DA ESQUERDA ================= */}
+        <main className="coluna-esquerda">
+          
+          {/* Container com os cards sobre os produtos */}
+          <div className="produtos-container">
+            <h2 className="titulo-cardapio">Cardápio</h2>
+            <div className="produtos-cards-wrapper">
+              <CardProduto nome="X-Burguer" preco="15,90" descricao="Hambúrguer artesanal grelhado, muito queijo prato derretido e maionese especial da casa no pão brioche selado." imagem="https://images.pexels.com/photos/33673825/pexels-photo-33673825.jpeg"/>
+              <CardProduto nome="X-Salada" preco="17,90" descricao="Blend bovino suculento, queijo derretido, alface americana fresca, tomates selecionados e molho artesanal." imagem="https://images.pexels.com/photos/3738730/pexels-photo-3738730.jpeg"/>
+              <CardProduto nome="Refrigerante" preco="6,00" descricao="Lata de 350ml trincando de gelada. Escolha o seu sabor favorito para acompanhar o seu lanche." imagem="https://images.pexels.com/photos/5860659/pexels-photo-5860659.jpeg"/>
+            </div>
           </div>
-      </div>
 
-      {/* Container com os cards sobre o caixa e o valor total */}
-      <div className="contador-container">
-        <h2 className="titulo-contador">Caixa</h2>
-        <div className="contador-cards-wrapper">
-          <Contador nome="X-Burguer" preco={15.90} onPrecoChange={atualizarTotalGeral}/>
-          <Contador nome="X-Salada" preco={17.90} onPrecoChange={atualizarTotalGeral}/>
-          <Contador nome="Refrigerante" preco={6.00} onPrecoChange={atualizarTotalGeral}/>
-        </div>
+          {/* Container com os cards sobre o caixa e o valor total */}
+          <div className="contador-container">
+            <h2 className="titulo-contador">Caixa</h2>
+            <div className="contador-cards-wrapper">
+              <Contador nome="X-Burguer" preco={15.90} onPrecoChange={atualizarTotalGeral}/>
+              <Contador nome="X-Salada" preco={17.90} onPrecoChange={atualizarTotalGeral}/>
+              <Contador nome="Refrigerante" preco={6.00} onPrecoChange={atualizarTotalGeral}/>
+            </div>
 
-        <div className="total-geral-caixa">
-          <h3>Total do Pedido: R$ {totalGeral.toFixed(2).replace('.', ',')}</h3>
-        </div>
+            <div className="total-geral-caixa">
+              <h3>Total do Pedido: R$ {totalGeral.toFixed(2).replace('.', ',')}</h3>
+            </div>
+          </div>
+
+        </main>
+
+        {/* ================= COLUNA DA DIREITA ================= */}
+        <aside className="coluna-direita">
+          
+          {/* Caixa de Login no topo da coluna da direita */}
+          <div className="login-sidebar">
+            <Login login="Login"/>
+          </div>
+
+          {/* Container com os cards sobre os funcionários logo abaixo do Login */}
+          <div className="funcionarios-container">
+              <h2 className="titulo-funcionarios">Funcionários</h2>
+              <div className="funcionarios-cards-wrapper">
+                <Funcionario nome="Alexandre Gaspari" cargo="Atendente" fotoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPSvxlMQ0yxOc2hXkochHyY51xkZsBxKFDHw&s"/>
+                <Funcionario nome="Cesar Stati" cargo="Gerente" fotoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwU2daGPdMx-ZYsHz8DuHeFBi_cqZtpUGF9Q&s"/>
+                <Funcionario nome="Donathan Goncalves" cargo="Chef de Cozinha" fotoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDxliqPfoR4K-WKT7JoGovyVi794g6FrtKXQ&s"/>
+              </div>
+          </div>
+
+        </aside>
+
       </div>
-      
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
